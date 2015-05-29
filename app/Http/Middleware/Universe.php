25f260a86298a476a736b38b1b8417c54d063b5a
Christian Wederka <info@christian-wederka.de>
2015-05-30 01:02:57 +0200

    "Universe" middleware added

---
+++ app/Http/Middleware/Universe.php
@@ -0,0 +1,31 @@
+<?php
+	namespace Eternal\Http\Middleware;
+
+	use Closure;
+	use Config;
+	use Session;
+	use Illuminate\Routing\Route;
+
+	class Universe {
+
+		private $route;
+
+		public function __construct(Route $route) {
+			$this->route = $route;
+		}
+
+		public function handle($request, Closure $next) {
+
+			$universe = $this->route->getParameter('universe');
+			if(!empty($universe)) {
+				Config::set('database.default', $universe);
+				Session::put('universe', $universe);
+
+				$this->route->forgetParameter('universe');
+			} else {
+				return redirect()->guest(Config::get('app.url'));
+			}
+
+			return $next($request);
+		}
+	}
