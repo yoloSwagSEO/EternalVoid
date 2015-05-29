d9911b84ca4a6e9dc72f5370440fab9178377624
Christian Wederka <info@christian-wederka.de>
2015-05-30 01:01:07 +0200

    Catch all method added to display public pages

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,10 +1,20 @@
 <?php
 	namespace Eternal\Http\Controllers;
 
+	use Hash;
+
 	class IndexController extends Controller {
 
-		public function getIndex() {
-			return view('pages.index.index');
+		public function getPage($page = '') {
+			return view('pages.index.'.(empty($page) ? 'index' : $page));
 		}
 
-}
+		public function getHash($value) {
+			return Hash::make($value);
+		}
+
+		public function missingMethod($parameters = array()) {
+			$parameters = explode('/', $parameters);
+			return call_user_func_array(array($this,'getPage'), $parameters);
+		}
+	}
