b9ac97afc4483994ff1068373f862c0c26ba13a9
Christian Wederka <info@christian-wederka.de>
2015-05-30 00:58:30 +0200

    Separate database config for game universe added

--- config/database.php
+++ config/database.php
@@ -64,6 +64,18 @@ return [
 			'strict'    => false,
 		],
 
+		'pheonix' => [
+			'driver'    => 'mysql',
+			'host'      => env('DB_HOST', 'localhost'),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => 'eternal_',
+			'strict'    => false,
+		],
+
 		'pgsql' => [
 			'driver'   => 'pgsql',
 			'host'     => env('DB_HOST', 'localhost'),
