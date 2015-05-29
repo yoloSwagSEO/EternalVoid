73d6df1eceacf9e3c717edc82335f938de8ade14
Christian Wederka <info@christian-wederka.de>
2015-05-30 01:01:33 +0200

    Registered "Universe" middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends HttpKernel {
 		'auth' => 'Eternal\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'Eternal\Http\Middleware\RedirectIfAuthenticated',
+		'universe' => 'Eternal\Http\Middleware\Universe',
 	];
 
 }
