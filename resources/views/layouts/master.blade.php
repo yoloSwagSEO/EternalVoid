1bfbfeb0bb6c441c50a9021a754dd242dc62b193
Christian Wederka <info@christian-wederka.de>
2015-05-30 01:01:51 +0200

    Updated master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,22 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ Config::get('app.locale') }}">
 	<head>
 		<meta charset="UTF-8">
 		<title>Eternal Void :: @yield('pagetitle')</title>
-		{!! HTML::style('http://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900') !!}
+		{!! HTML::style('http://fonts.googleapis.com/css?family=Roboto:900,700,400,500,300,100|Roboto+Condensed:400,300,700') !!}
+		{!! HTML::style('css/font-awesome.min.css') !!}
 		{!! HTML::style('css/bootstrap.min.css') !!}
 		{!! HTML::style('css/bootstrap-theme.min.css') !!}
-		{!! HTML::style('css/font-awesome.min.css') !!}
+		{!! HTML::style('css/helpers.min.css') !!}
+		{!! HTML::style('css/eternalvoid.css') !!}
 		@yield('pagecss')
 
 	</head>
 	<body>
-		<div class="container-fluid">
-			@yield('content')
+		<div class="container">
+			<h1><a href="/"><span>eternalvoid.de</span></a></h1>
+			<nav>
+				<ul>
+					<li><a href="/">Entwickler-Blog</a></li>
+					<li><a href="/earlyaccess">Early Access</a></li>
+					<li><a href="/screenshots">Screenshots</a></li>
+					<li><a href="/projektstatus">Projektstatus</a></li>
+				</ul>
+			</nav>
+			<div class="wrapper">
+				<div class="wrapper_top"></div>
+				<div class="wrapper_inner">
+					@yield('content')
 
+				</div>
+				<div class="wrapper_btm"></div>
+			</div>
 		</div>
 		{!! HTML::script('js/jquery.js') !!}
 		{!! HTML::script('js/bootstrap.min.js') !!}
+		{!! HTML::script('js/main/eternalvoid.js') !!}
 		@yield('pagejs')
 
 	</body>
