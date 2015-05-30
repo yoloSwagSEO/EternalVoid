803faf0bd58cb58649e8eca0c0224d7a01837e5f
Christian Wederka <info@christian-wederka.de>
2015-05-30 02:42:09 +0200

    #1 Blog post added

--- resources/views/pages/index/index.blade.php
+++ resources/views/pages/index/index.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.master')
 @section('content')
-
+	<h4>30.05.2015 &mdash; Datenverlust und der langsame Wiederaufbau</h4>
+	<p>
+		Sorry für die lange Downtime des Projektes.
+	</p>
+	<p>
+		Ich hatte leider mit einem Datenverlust, der die Arbeit von gut 6 Wochen zunichte gemacht und teilweise durch
+		meine IDE verursacht wurde, zu kämpfen. Ich war deswegen ziemlich gefrustet und hatte auch ehrlichgesagt keine Lust von
+		verschiedenen Stellen wieder alles zusammenzusuchen. Dazu kam noch, dass durch die Veröffentlichung von Laravel 5
+		im Januar 2015 nicht unerhebliche Umbauarbeiten erforderlich wurden, was meine Lust noch weiter in den Keller gedrückt hat.
+	</p>
+	<p>
+		Aber irgendwann ist auch die Talsohle erreicht und so geht es jetzt wieder aufwärts. Es wird allerdings einige Zeit
+		dauern, bis ich wieder auf dem Stand bin, zumal ich gleich die Umbauarbeiten für Laravel 5 durchführe. Gleichzeitig
+		habe ich mich dazu entschieden den <a href="https://github.com/chrischi1989/EternalVoid">Code auf GitHub</a> zu stellen.
+		Einerseits weil's für mich eine Art Sicherung darstellt nicht nochmal was zu verlieren und andererseits weil ich
+		so die Hoffnung habe, dass sich noch andere Entwickler beteiligen.
+	</p>
+	<p>
+		Als Hinweis möchte ich noch sagen, dass das Projekt unter MIT Lizenz steht. Zu gut Deutsch heißt dass ihr mit dem
+		Code machen könnt was ihr wollt, ich aber nicht dazu verpflichtet bin, auch nur irgendeinen Finger krum zu machen,
+		wenn ihr nen Fehler findet oder mit irgendwas unzufrieden seid. Wer sich beteiligen möchte: Nur zu. Macht nen Fork,
+		bastelt dran rum und macht Pull-Requests.
+	</p>
 @stop
