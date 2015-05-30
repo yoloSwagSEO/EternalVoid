8e0ad33e36cda9b34ec69efe61fa1425707042e7
Christian Wederka <info@christian-wederka.de>
2015-05-31 00:27:52 +0200

    Added registration functionality

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,15 @@
 	namespace Eternal\Http\Controllers;
 
 	use Eternal\Models\User;
+	use Eternal\Models\UserProfile;
+	use Eternal\Models\Planet;
+	use Eternal\Models\Resource;
+	use Eternal\Models\Research;
+	use Eternal\Models\Production;
+	use Eternal\Models\Building;
+	use Eternal\Models\Defense;
+	use Eternal\Models\Unit;
+
 
 	use Session;
 	use Request;
@@ -9,9 +18,35 @@
 	class UsersController extends Controller {
 
 		private $user;
+		private $userProfile;
+		private $planet;
+		private $resource;
+		private $research;
+		private $production;
+		private $building;
+		private $defense;
+		private $unit;
 
-		public function __construct(User $user) {
-			$this->user = $user;
+		public function __construct(
+			User $user,
+			UserProfile $userProfile,
+			Planet $planet,
+			Resource $resource,
+			Research $research,
+			Production $production,
+			Building $building,
+			Defense $defense,
+			Unit $unit
+		) {
+			$this->user 	   = $user;
+			$this->userProfile = $userProfile;
+			$this->planet 	   = $planet;
+			$this->resource    = $resource;
+			$this->research	   = $research;
+			$this->production  = $production;
+			$this->building	   = $building;
+			$this->defense	   = $defense;
+			$this->unit		   = $unit;
 		}
 
 		public function getRegister() {
@@ -20,7 +55,35 @@
 
 		public function postRegister() {
 			if($this->user->register()) {
-				return redirect('users/register')->withSuccess(trans('users.messages.register_success'));
+
+				// Pick a random planet
+				$planet = $this->planet->random();
+
+				// Create default user profile
+				$this->userProfile->add($this->user);
+
+				// Set default user techs
+				$this->research->add($this->user);
+
+				// Set users start planet
+				$this->planet->setUser($planet, $this->user);
+
+				// Set default planet resources
+				$this->resource->add($planet);
+
+				// Set default planet production
+				$this->production->add($planet);
+
+				// Set default planet buildings
+				$this->building->add($planet);
+
+				// Set default planet defenses
+				$this->defense->add($planet);
+
+				// Set default planet units
+				$this->unit->add($planet);
+
+				return redirect('earlyaccess')->withSuccess(trans('users.messages.register_success'));
 			}
 
 			return redirect('earlyaccess')->withErrors($this->user->validator)->withInput();
