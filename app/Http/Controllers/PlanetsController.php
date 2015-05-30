1b62d86b6a9e2b232a7167336756f2708217b793
Christian Wederka <info@christian-wederka.de>
2015-05-31 00:29:20 +0200

    Returns planet overview view object on getIndex()

--- app/Http/Controllers/PlanetsController.php
+++ app/Http/Controllers/PlanetsController.php
@@ -1,10 +1,16 @@
 <?php
 	namespace Eternal\Http\Controllers;
 
-	class PlanetsController extends Controller {
+	use Eternal\Libraries\Main;
+
+	class PlanetsController extends MainController {
+
+		public function __construct(Main $main) {
+			parent::__construct($main);
+		}
 
 		public function getIndex() {
-
+			return view('pages.game.'.$this->game['viewpath'].'.planet');
 		}
 
 	}
