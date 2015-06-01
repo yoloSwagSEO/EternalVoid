dac3575ddd7f9aa3ca6583435cae2d16f3f01462
Christian Wederka <info@christian-wederka.de>
2015-06-01 02:37:48 +0200

    Relations and method for setting the last updated timestamp added

--- app/Models/Planet.php
+++ app/Models/Planet.php
@@ -24,7 +24,7 @@
 			return $this->whereBonus(0)->whereUserId(0)->orderBy(DB::raw('RAND()'))->take(1)->first();
 		}
 
-		public function setUser($planet, $user) {
+		public function setUser(Planet $planet, User $user) {
 			$planet->user_id       = $user->id;
 			$planet->base_id       = 1;
 			$planet->planetname    = Crypt::encrypt('Planet');
@@ -34,12 +34,29 @@
 			return $planet->save();
 		}
 
+		public function setLastupdate(Planet $planet) {
+			$planet->lastupdate_at = time();
+			return $planet->save();
+		}
+
 		public function production() {
 			return $this->hasOne('Eternal\Models\Production', 'planet_id', 'id');
 		}
 
 		public function resources() {
 			return $this->hasOne('Eternal\Models\Resource', 'planet_id', 'id');
+		}
+
+		public function buildings() {
+			return $this->hasOne('Eternal\Models\Building', 'planet_id', 'id');
+		}
+
+		public function defenses() {
+			return $this->hasOne('Eternal\Models\Defense', 'planet_id', 'id');
+		}
+
+		public function units() {
+			return $this->hasOne('Eternal\Models\Unit', 'planet_id', 'id');
 		}
 
 	}
