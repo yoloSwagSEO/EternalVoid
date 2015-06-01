ccb546bc596be45f544842d06f9b9c6131fc0a0a
Christian Wederka <info@christian-wederka.de>
2015-06-01 05:27:22 +0200

    Toggle for showing planet images on galaxy page

--- app/Http/Controllers/OptionsController.php
+++ app/Http/Controllers/OptionsController.php
@@ -1,6 +1,21 @@
 <?php
 	namespace Eternal\Http\Controllers;
 
-	class OptionsController extends Controller {
+	use Eternal\Libraries\Main;
+
+	class OptionsController extends MainController {
+
+		public function __construct(Main $main) {
+			parent::__construct($main);
+		}
+
+		public function getPlanetimages() {
+			if($this->user->profile->setPlanetimages($this->user->profile)) {
+				return redirect()->back();
+			}
+
+			return redirect()->back();
+		}
+
 
 	}
