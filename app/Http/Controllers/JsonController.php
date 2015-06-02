a8075d48b3212e24dd643538dd8f5a7330f8c3c4
Christian Wederka <info@christian-wederka.de>
2015-06-02 02:43:52 +0200

    Method for returning JSON object of usernames (used by user message system)

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -1,6 +1,32 @@
 <?php
 	namespace Eternal\Http\Controllers;
 
+	use Eternal\Models\User;
+
+	use Request;
+
 	class JsonController extends Controller {
+
+		private $user;
+
+		public function __construct(User $user) {
+			$this->user = $user;
+		}
+
+		public function getReceivers() {
+			if(Request::ajax()) {
+
+				$users     = $this->user->likeByUsername(Request::get('term'));
+				$usernames = [];
+
+				foreach($users as $user) {
+					$usernames[] = $user->username;
+				}
+
+				return response()->json($usernames);
+			}
+
+			return response('', 403);
+		}
 
 	}
