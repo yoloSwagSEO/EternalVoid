b491cfe2064bde1f00c90bda5d8e7ef6de109dc3
Christian Wederka <info@christian-wederka.de>
2015-06-08 20:44:15 +0200

    fixed indention

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -1,58 +1,58 @@
 <?php
-    namespace Eternal\Models;
+	namespace Eternal\Models;
 
-    use Carbon\Carbon;
+	use Carbon\Carbon;
 
-    class Event extends Base {
+	class Event extends Base {
 
-        protected $table = 'events';
-        protected $dates = [
-            'finished_at'
-        ];
+		protected $table = 'events';
+		protected $dates = [
+			'finished_at'
+		];
 
-        public function read($userId, $type, $planetId = '') {
-            if(!empty($planetId)) {
-                return $this->where([
-                    'user_id'   => $userId,
-                    'planet_id' => $planetId,
-                    'type'      => $type,
-                ])->get();
-            }
+		public function read($userId, $type, $planetId = '') {
+			if(!empty($planetId)) {
+				return $this->where([
+					'user_id'   => $userId,
+					'planet_id' => $planetId,
+					'type'      => $type,
+				])->get();
+			}
 
-            return $this->where([
-                'user_id' => $userId,
-                'type'    => $type,
-            ])->get();
-        }
+			return $this->where([
+				'user_id' => $userId,
+				'type'    => $type,
+			])->get();
+		}
 
-        public function add($data, $type) {
-            unset($data['image']);
-            unset($data['description']);
+		public function add($data, $type) {
+			unset($data['image']);
+			unset($data['description']);
 
-            $this->plt->resources->aluminium -= isset($data['aluminium']) ? $data['aluminium'] : $this->plt->resources->aluminium;
-            $this->plt->resources->titan -= isset($data['titan']) ? $data['titan'] : $this->plt->resources->titan;
-            $this->plt->resources->silizium -= isset($data['silizium']) ? $data['silizium'] : $this->plt->resources->silizium;
-            $this->plt->resources->arsen -= isset($data['arsen']) ? $data['arsen'] : $this->plt->resources->silizium;
-            $this->plt->resources->wasserstoff -= isset($data['wasserstoff']) ? $data['wasserstoff'] : $this->plt->resources->wasserstoff;
-            $this->plt->resources->antimaterie -= isset($data['antimaterie']) ? $data['antimaterie'] : $this->plt->resources->antimaterie;
-            $this->plt->resources->save();
+			$this->plt->resources->aluminium -= isset($data['aluminium']) ? $data['aluminium'] : $this->plt->resources->aluminium;
+			$this->plt->resources->titan -= isset($data['titan']) ? $data['titan'] : $this->plt->resources->titan;
+			$this->plt->resources->silizium -= isset($data['silizium']) ? $data['silizium'] : $this->plt->resources->silizium;
+			$this->plt->resources->arsen -= isset($data['arsen']) ? $data['arsen'] : $this->plt->resources->silizium;
+			$this->plt->resources->wasserstoff -= isset($data['wasserstoff']) ? $data['wasserstoff'] : $this->plt->resources->wasserstoff;
+			$this->plt->resources->antimaterie -= isset($data['antimaterie']) ? $data['antimaterie'] : $this->plt->resources->antimaterie;
+			$this->plt->resources->save();
 
-            $this->user_id     = $this->usr->id;
-            $this->planet_id   = $this->plt->id;
-            $this->type        = $type;
-            $this->finished_at = Carbon::now()->addSeconds($data['time']);
-            $this->data        = serialize($data);
+			$this->user_id     = $this->usr->id;
+			$this->planet_id   = $this->plt->id;
+			$this->type        = $type;
+			$this->finished_at = Carbon::now()->addSeconds($data['time']);
+			$this->data        = serialize($data);
 
-            return $this->save();
-        }
+			return $this->save();
+		}
 
-        public function modify($event, $data) {
-            $event->data = serialize($data);
-            return $event->save();
-        }
+		public function modify($event, $data) {
+			$event->data = serialize($data);
+			return $event->save();
+		}
 
-        public function remove($event) {
-            return $event->delete();
-        }
+		public function remove($event) {
+			return $event->delete();
+		}
 
-    }
+	}
