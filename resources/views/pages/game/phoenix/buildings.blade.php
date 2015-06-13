5468ffbd9423909b676b6853783edff66d9b8a5a
Christian Wederka <info@christian-wederka.de>
2015-06-13 10:23:10 +0200

    Fixed indention + better responsive behavior

--- resources/views/pages/game/phoenix/buildings.blade.php
+++ resources/views/pages/game/phoenix/buildings.blade.php
@@ -7,46 +7,48 @@
 		<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			@if(!$current_build_jobs->isEmpty())
 
-				<div class="ccontainer mb20">
-					<div class="panel panel-default mb0">
-						<table class="table table-bordered table-hover">
-							<thead class="ttu">
-							<tr>
-								<th class="w15 pt10 pb10 pl5">Objekt</th>
-								<th class="w15 pt10 pb10 pl5">Benötigte Zeit</th>
-								<th colspan="4" class="w70 pt10 pb10 pl5">Fertigstellung</th>
-							</tr>
-							</thead>
-							<{{ $current_build_jobs->count() == 1 ? 'tfoot' : 'tbody' }}>
-							@foreach($current_build_jobs as $key => $build)
-								<?php $data = unserialize($build['data']); ?>
+			<div class="ccontainer mb20">
+				<div class="panel panel-default mb0">
+					<table class="table table-bordered table-hover">
+						<thead class="ttu">
+						<tr>
+							<th class="w15 pt10 pb10 pl5">Objekt</th>
+							<th class="w15 pt10 pb10 pl5">Benötigte Zeit</th>
+							<th colspan="4" class="w70 pt10 pb10 pl5">Fertigstellung</th>
+						</tr>
+						</thead>
+						<{{ $current_build_jobs->count() == 1 ? 'tfoot' : 'tbody' }}>
+						@foreach($current_build_jobs as $key => $build)
+						<?php $data = unserialize($build['data']); ?>
 
-								<tr>
-									<td class="p5 vmiddle">{{ $data['name'] }}</td>
-									<td class="p5 vmiddle">{{ $help->htime($data['time']) }}</td>
-									<td class="w15 p5 vmiddle"><span class="countdown" id="countdown-{{ str_random() }}" data-until="{{ $data['remaining'] }}">{{ $help->htime($data['remaining']) }}</span></td>
-									<td class="w15 p5 vmiddle">{{ $build->finished_at->format('d.m.Y - H:i:s') }}</td>
-									<td class="w75 p5 vmiddle">
-										<div class="progress mb0" data-time="{{ $data['time'] }}" data-remaining="{{ $data['remaining'] }}">
-											<div class="progress-bar progress-bar-success" role="progressbar" style="width: {{ round(100 - (($data['remaining'] / $data['time']) * 100), 2) }}%">
-												{{ round(100 - (($data['remaining'] / $data['time']) * 100),2) }}%
-											</div>
+							<tr>
+							<td class="p5 vmiddle">{{ $data['name'] }}</td>
+							<td class="p5 vmiddle">{{ $help->htime($data['time']) }}</td>
+							<td class="w15 p5 vmiddle"><span class="countdown" id="countdown-{{ str_random() }}" data-until="{{ $data['remaining'] }}">{{ $help->htime($data['remaining']) }}</span></td>
+								<td class="w15 p5 vmiddle">{{ $build->finished_at->format('d.m.Y - H:i:s') }}</td>
+								<td class="w75 p5 vmiddle">
+									<div class="progress mb0" data-time="{{ $data['time'] }}" data-remaining="{{ $data['remaining'] }}">
+										<div class="progress-bar progress-bar-success" role="progressbar" style="width: {{ round(100 - (($data['remaining'] / $data['time']) * 100), 2) }}%">
+											{{ round(100 - (($data['remaining'] / $data['time']) * 100),2) }}%
 										</div>
-									</td>
-									<td class="w5 text-center">
-										<a href="/buildings/cancel/{{ $build->id }}" title="Abbrechen"><i class="fa fa-times-circle fa-lg"></i></a>
-									</td>
-								</tr>
-							@endforeach
+									</div>
+								</td>
+								<td class="w5 text-center">
+									<a href="/buildings/cancel/{{ $build->id }}" title="Abbrechen"><i class="fa fa-times-circle fa-lg"></i></a>
+								</td>
+							</tr>
+						@endforeach
 
 						</{{ $current_build_jobs->count() == 1 ? 'tfoot' : 'tbody' }}>
-						</table>
-					</div>
+					</table>
 				</div>
+			</div>
 			@endif
+
+			<div class="row">
 			@foreach($buildings as $build => $building)
 
-				<div class="col-lg-2 col-md-2 col-sm-3 col-xs-4 p0">
+				<div class="col-lg-2 col-md-2 col-sm-3 col-xs-4 item">
 					<div class="gcontainer">
 						<div class="posrel">
 							<div class="overlay_top">{{ $building['name'] }}</div>
@@ -80,6 +82,7 @@
 				</div>
 			@endforeach
 
+			</div>
 		</div>
 	</div>
 @stop
