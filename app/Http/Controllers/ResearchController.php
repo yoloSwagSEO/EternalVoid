649dac844dee6241d602e65d88165478481658f7
Christian Wederka <info@christian-wederka.de>
2015-06-17 17:12:59 +0200

    Simplified code (removed some if-statements)

--- app/Http/Controllers/ResearchController.php
+++ app/Http/Controllers/ResearchController.php
@@ -91,36 +91,19 @@
         }
 
         private function setResources($tech) {
-            if(isset($tech['aluminium'])) {
-                $tech['aluminium'] = floor($tech['aluminium'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
-            }
-
-            if(isset($tech['titan'])) {
-                $tech['titan'] = floor($tech['titan'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
-            }
-
-            if(isset($tech['silizium'])) {
-                $tech['silizium'] = floor($tech['silizium'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
-            }
-
-            if(isset($tech['arsen'])) {
-                $tech['arsen'] = floor($tech['arsen'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
-            }
-
-            if(isset($tech['wasserstoff'])) {
-                $tech['wasserstoff'] = floor($tech['wasserstoff'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
-            }
-
-            if(isset($tech['antimaterie'])) {
-                $tech['antimaterie'] = floor($tech['antimaterie'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
-            }
+            $tech['aluminium'] = floor($tech['aluminium'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
+            $tech['titan'] = floor($tech['titan'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
+            $tech['silizium'] = floor($tech['silizium'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
+            $tech['arsen'] = floor($tech['arsen'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
+            $tech['wasserstoff'] = floor($tech['wasserstoff'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
+            $tech['antimaterie'] = floor($tech['antimaterie'] * pow(($this->research->{$tech['key']} + ($tech['level'] + 1)), 1.4));
 
             return $tech;
         }
 
         private function setResearchTime($tech) {
-            $modifierOne = isset($tech['aluminium']) ? $tech['aluminium'] : 0;
-            $modifierTwo = isset($tech['titan']) ? $tech['titan'] : 0;
+            $modifierOne = $tech['aluminium'];
+            $modifierTwo = $tech['titan'];
 
             $tech['time'] = floor(((($modifierOne + $modifierTwo) / (1000 * (1 + $this->planet->buildings->forschungszentrum))) * 3600) / $this->game['speed']);
 
@@ -129,72 +112,25 @@
 
         private function setResearchPermission($tech) {
             $tech['build'] = true;
-
-            if(isset($tech['aluminium'])) {
-                $tech['build'] =  $tech['aluminium'] <= $this->resources->aluminium && $tech['build'] ? true : false;
-            }
-
-            if(isset($tech['titan'])) {
-                $tech['build'] = $tech['titan'] <= $this->resources->titan && $tech['build'] ? true : false;
-            }
-
-            if(isset($tech['silizium'])) {
-                $tech['build'] = $tech['silizium'] <= $this->resources->silizium && $tech['build'] ? true : false;
-            }
-
-            if(isset($tech['arsen'])) {
-                $tech['build'] = $tech['arsen'] <= $this->resources->arsen && $tech['build'] ? true : false;
-            }
-
-            if(isset($tech['wasserstoff'])) {
-                $tech['build'] = $tech['wasserstoff'] <= $this->resources->wasserstoff && $tech['build'] ? true : false;
-            }
-
-            if(isset($tech['antimaterie'])) {
-                $tech['build'] = $tech['antimaterie'] <= $this->resources->antimaterie && $tech['build'] ? true : false;
-            }
+            $tech['build'] = $tech['aluminium'] <= $this->resources->aluminium && $tech['build'] ? true : false;
+            $tech['build'] = $tech['titan'] <= $this->resources->titan && $tech['build'] ? true : false;
+            $tech['build'] = $tech['silizium'] <= $this->resources->silizium && $tech['build'] ? true : false;
+            $tech['build'] = $tech['arsen'] <= $this->resources->arsen && $tech['build'] ? true : false;
+            $tech['build'] = $tech['wasserstoff'] <= $this->resources->wasserstoff && $tech['build'] ? true : false;
+            $tech['build'] = $tech['antimaterie'] <= $this->resources->antimaterie && $tech['build'] ? true : false;
 
             return $tech;
         }
 
         private function setResourceTime($tech) {
-            $data = [];
-
-            if(isset($tech['aluminium'])) {
-                $data[] = floor(($tech['aluminium'] - $this->resources->aluminium) / ($this->production->aluminium + $this->game['aluminium']));
-            }
-
-            if(isset($tech['titan'])) {
-                $data[] = floor(($tech['titan'] - $this->resources->titan) / ($this->production->titan + $this->game['titan']));
-            }
-
-            if(isset($tech['silizium'])) {
-                $data[] = floor(($tech['silizium'] - $this->resources->silizium) / ($this->production->silizium + $this->game['silizium']));
-            }
-
-            if(isset($tech['arsen'])) {
-                if($this->production->arsen > 0) {
-                    $data[] = floor(($tech['arsen'] - $this->resources->arsen) / $this->production->arsen);
-                } else {
-                    $data[] = 0;
-                }
-            }
-
-            if(isset($tech['wasserstoff'])) {
-                if($this->production->wasserstoff > 0) {
-                    $data[] = floor(($tech['wasserstoff'] - $this->resources->wasserstoff) / $this->production->wasserstoff);
-                } else {
-                    $data[] = 0;
-                }
-            }
-
-            if(isset($tech['antimaterie'])) {
-                if($this->production->antimaterie > 0) {
-                    $data[] = floor(($tech['antimaterie'] - $this->resources->antimaterie) / $this->production->antimaterie);
-                } else {
-                    $data[] = 0;
-                }
-            }
+            $data = [
+                floor(($tech['aluminium'] - $this->resources->aluminium) / ($this->production->aluminium + $this->game['aluminium'])),
+                floor(($tech['titan'] - $this->resources->titan) / ($this->production->titan + $this->game['titan'])),
+                floor(($tech['silizium'] - $this->resources->silizium) / ($this->production->silizium + $this->game['silizium'])),
+                $this->production->arsen > 0 ? floor(($tech['arsen'] - $this->resources->arsen) / $this->production->arsen) : 0,
+                $this->production->wasserstoff > 0 ? floor(($tech['wasserstoff'] - $this->resources->wasserstoff) / $this->production->wasserstoff) : 0,
+                $this->production->antimaterie > 0 ? floor(($tech['antimaterie'] - $this->resources->antimaterie) / $this->production->antimaterie) : 0
+            ];
 
             $tech['restime'] = max($data);
 
